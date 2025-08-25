feb923fb0c4b2219e4e77692a4a550f4def89131
Maximilian <scientificresearch309@gmail.com>
2025-08-26 00:28:23 +0200

    import changed to require

--- server.js
+++ server.js
@@ -1,42 +1,26 @@
 'use strict';
 
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj };
-}
+var express = require('express');
+var dotenv = require('dotenv');
+var cors = require('cors');
 
-import _express from 'express';
+dotenv.config();
+cors();
 
-var _express2 = _interopRequireDefault(_express);
-
-import _dotenv from 'dotenv';
-
-var _dotenv2 = _interopRequireDefault(_dotenv);
-
-import _cors from 'cors';
-
-var _cors2 = _interopRequireDefault(_cors);
-
-_dotenv2['default'].config();
-(0, _cors2['default'])();
-
-var api = (0, _express2['default'])();
+var api = express();
 var PORT = process.env.PORT;
-api.use((0, _cors2['default'])());
+api.use(cors());
 
-var scores = [
-  {
-    user: 'EJT',
-    score: '01842',
-  },
-  {
-    user: 'PPB',
-    score: '01812',
-  },
-  {
-    user: 'EJT',
-    score: '01442',
-  },
-];
+var scores = [{
+  user: 'EJT',
+  score: '01842'
+}, {
+  user: 'PPB',
+  score: '01812'
+}, {
+  user: 'EJT',
+  score: '01442'
+}];
 
 api.get('/', function (req, res) {
   res.send('Test API works!');
